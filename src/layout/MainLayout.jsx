02b8d88b25cd08d59fa9031c63265e35c074a7ef
chasyuss <cotn963@gmail.com>
2025-04-09 11:04:31 +0900

    style: content 부분 설정 정의

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../common/Header';
+import styled from 'styled-components';
 
 const MainLayout = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <ContentWrapper>
+        <Outlet />
+      </ContentWrapper>
     </>
   );
 };
 
 export default MainLayout;
+
+const ContentWrapper = styled.div`
+  margin-left: 160px;
+  margin-right: 175px;
+`;
