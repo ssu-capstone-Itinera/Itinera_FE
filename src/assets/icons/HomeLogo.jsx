1f3829b020a643d7cc7ab79d506bc87c1006752e
chasyuss <cotn963@gmail.com>
2025-04-09 11:03:51 +0900

    style: header ui 구현

---
+++ src/assets/icons/HomeLogo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeLogo = () => {
+  return (
+    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clip-path="url(#clip0_334_1004)">
+        <path d="M10.7026 20V14H14.7026V20H19.7026V12H22.7026L12.7026 3L2.70264 12H5.70264V20H10.7026Z" fill="#12464C" />
+      </g>
+      <defs>
+        <clipPath id="clip0_334_1004">
+          <rect width="24" height="24" fill="white" transform="translate(0.702637)" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export default HomeLogo
