7ea6964e9affe4a92c22d33b6552999bad7a0d50
rlgo0125 <rlgo00125@gmail.com>
2025-05-10 00:53:45 +0900

    아이콘 추가 및 변경

---
+++ src/assets/icons/KakaoIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const KakaoIcon = () => {
+  return (
+    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0001 1.6001C5.37225 1.6001 0 5.75073 0 10.8698C0 14.0535 2.07788 16.8601 5.24205 18.5294L3.91072 23.3928C3.7931 23.8226 4.28457 24.1651 4.66197 23.9161L10.4978 20.0644C10.9903 20.112 11.4908 20.1397 12.0001 20.1397C18.6274 20.1397 24 15.9892 24 10.8698C24 5.75073 18.6274 1.6001 12.0001 1.6001Z" fill="black"/>
+    </svg>
+
+  )
+}
+
+export default KakaoIcon
+
